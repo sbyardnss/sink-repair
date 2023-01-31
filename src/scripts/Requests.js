734cdecd1c37b2129cf3bbe1ca6180cdf5e6c118
Mary Williams <sbyardnss@gmail.com>
2023-01-31 15:09:10 -0600

    request list fixed. order submit works

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -1,17 +1,42 @@
 import { getRequests } from "./dataAccess.js"
 
 
-const convertRequestsToList = () => {
+// const convertRequestsToList = () => {
+//     const requests = getRequests()
+//     let html = ""
+//     for (const request of requests) {
+//         html += `<li>${request.description}</li>`
+//     }
+//     return html
+// }
+
+// export const Requests = () => {
+//     const requests = getRequests()
+//     let html = `
+//         <ul>
+//             ${
+//                 requests.map(convertRequestsToList).join("")
+//             }
+//         </ul>
+//     `
+//     return html
+// }
+
+const convertRequestsToList = (obj) => {
     const requests = getRequests()
     let html = ""
-    for (const request of requests) {
-        html += `<li>${request.description}</li>`
-    }
+    html += `<li>${obj.description}</li>`
+
+    // OLD CODE THAT TRIPLED LIST BECAUSE YOU ARE EFFECTIVELY ITERRATING TWICE
+    // for (const request of requests) {
+        // html += `<li>${obj.description}</li>`
+    // }
     return html
 }
 
 export const Requests = () => {
     const requests = getRequests()
+    const requestList = ""
     let html = `
         <ul>
             ${
